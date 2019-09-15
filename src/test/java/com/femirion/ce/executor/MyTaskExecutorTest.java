4fe48d719593871c146498d55cccd5ae66d403e2
steam <femirion@rambler.ru>
2019-09-15 23:11:34 +0300

    add test for executing in two threads

--- src/test/java/com/femirion/ce/executor/MyTaskExecutorTest.java
+++ src/test/java/com/femirion/ce/executor/MyTaskExecutorTest.java
@@ -5,14 +5,30 @@ import org.junit.Test;
 
 import java.time.LocalDateTime;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertEquals;
 
 public class MyTaskExecutorTest {
 
+
     @Test
-    public void execute() throws ExecutionException, InterruptedException {
+    public void executeInTwoThread() throws Exception {
+        // given
+        MyTaskExecutor<String> subj = new MyTaskExecutor<>();
+
+
+        // then
+        CompletableFuture<String> result1 = subj.execute(new Task<>(LocalDateTime.now(), () -> "test1"));
+        CompletableFuture<String> result2 = subj.execute(new Task<>(LocalDateTime.now(), () -> "test2"));
+
+
+        // when
+        assertEquals("test1", result1.get());
+        assertEquals("test2", result2.get());
+    }
+
+    @Test
+    public void execute() throws Exception {
         // given
         MyTaskExecutor<String> subj = new MyTaskExecutor<>();
 
